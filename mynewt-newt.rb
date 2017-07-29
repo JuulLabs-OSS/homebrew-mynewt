f6b65201be8d66b622d4250c564dfca96c587639
cwanda <wanda@happycity.com>
2017-07-28 17:21:31 -0700

    added sierra bottles

--- mynewt-newt.rb
+++ mynewt-newt.rb
@@ -11,6 +11,7 @@ class MynewtNewt < Formula
 #     root_url "https://github.com/runtimeco/binary-releases/raw/master/mynewt-newt-tools_1.1.0"
     root_url "https://github.com/cwanda/homebrew-testmynewt/raw/master"
     cellar :any_skip_relocation
+    sha256 "68221671cc744d90ae06235e49a8638dd75a36677d27baea7f16ef1482a4127e" => :sierra
     sha256 "3092023e660ac252c5ad07166f9ea6ebb31f665c4c1d496698b7982de9d9dc08" => :yosemite
     sha256 "83e119596ffa17c1f828686245428082e0cb4605680c93ea01155994c82595ce" => :el_capitan
   end
--- mynewt-newtmgr@1.0.rb
+++ mynewt-newtmgr@1.0.rb
@@ -11,6 +11,7 @@ class MynewtNewtmgrAT10 < Formula
 #    root_url "https://github.com/runtimeco/binary-releases/raw/master/mynewt-newt-tools_1.0.0"
     root_url "https://github.com/cwanda/homebrew-testmynewt/raw/master"
     cellar :any_skip_relocation
+    sha256 "63b1be15469ddbacb31ccc10eb0f8314f8248b3c2d94b6e31f1259c361ec2128" => :sierra
     sha256 "f824ccea8eae3ac293f450d5e0d4d8a4b1e928394f4d979ab740fe67b8c97ec0" => :yosemite
     sha256 "e8b84cca0cd5a92dff33e343ca9de87bdfed51353ccfbeadb8aea6c63704c114" => :el_capitan
   end
