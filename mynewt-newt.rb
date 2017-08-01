7f57526d5eafa1e5b58f1fb9320f4f6c181db166
cwanda <wanda@happycity.com>
2017-07-31 17:41:07 -0700

    1.1.0 and @1.0 brew formulas for newt and newtmgr - Updated formulas to 1.1.0. - Added @1.0 formulas so user can install 1.0.0 newt and newtmgr

--- mynewt-newt.rb
+++ mynewt-newt.rb
@@ -1,16 +1,18 @@
 class MynewtNewt < Formula
   desc "Package, build and installation system for Mynewt OS applications"
   homepage "https://mynewt.apache.org"
-  url "https://github.com/apache/incubator-mynewt-newt/archive/mynewt_1_0_0_tag.tar.gz"
-  version "1.0.0"
-  sha256 "699239691b6fe2e5bb0bb24727cd56740400e6074a369756b890ea7ec290d1bd"
+  url "https://github.com/apache/mynewt-newt/archive/mynewt_1_1_0_tag.tar.gz"
+  version "1.1.0"
+  sha256 "66a90b54034255e073206cc27d9d1a63b76d6dd56a847e4f224a50e3de942aad"
 
-  head "https://github.com/apache/incubator-mynewt-newt.git"
+  head "https://github.com/apache/mynewt-newt.git"
 
   bottle do
-    root_url "https://github.com/runtimeco/binary-releases/raw/master/mynewt-newt-tools_1.0.0"
+    root_url "https://github.com/runtimeco/binary-releases/raw/master/mynewt-newt-tools_1.1.0"
     cellar :any_skip_relocation
-    sha256 "bbcd73426e3807261102d59687cdf77e369a6d172a61394351c1ffc4ffd27396" => :mavericks_or_later
+    sha256 "68221671cc744d90ae06235e49a8638dd75a36677d27baea7f16ef1482a4127e" => :sierra
+    sha256 "83e119596ffa17c1f828686245428082e0cb4605680c93ea01155994c82595ce" => :el_capitan
+    sha256 "3092023e660ac252c5ad07166f9ea6ebb31f665c4c1d496698b7982de9d9dc08" => :yosemite
   end
 
   depends_on "go" => :build
@@ -31,6 +33,6 @@ class MynewtNewt < Formula
 
   test do
     # Compare newt version string
-    assert_equal "1.0.0", shell_output("#{bin}/newt version").split.last
+    assert_equal "1.1.0", shell_output("#{bin}/newt version").split.last
   end
 end
---
+++ mynewt-newt@1.0.rb
@@ -0,0 +1,38 @@
+class MynewtNewtAT10 < Formula
+  desc "Package, build and installation system for Mynewt OS applications (1.0)"
+  homepage "https://mynewt.apache.org"
+  url "https://github.com/apache/mynewt-newt/archive/mynewt_1_0_0_tag.tar.gz"
+  version "1.0.0"
+  sha256 "6c39967881357b228c54683c1eaffb47662edbafee7e07c1a27351857a54b5dd"
+
+  bottle do
+    root_url "https://github.com/runtimeco/binary-releases/raw/master/mynewt-newt-tools_1.0.0"
+    cellar :any_skip_relocation
+    sha256 "d79f086e378b58b239c4bdf055200bf8d9a85b5068752e4da475999c2d4596fe" => :sierra
+    sha256 "6af9fd1e94593b73ee9fcd6511fcf648005db413a3a6cc76709a8b8c219c5a1f" => :el_capitan
+    sha256 "1ecb25e903ae3dcb35461455babaccf7c061b95a1a95f0bc6b16d646052c4546" => :yosemite
+  end
+   
+  keg_only :versioned_formula
+
+  depends_on "go" => :build
+  depends_on :arch => :x86_64
+
+  def install
+    contents = Dir["{*,.git,.gitignore}"]
+    gopath = buildpath/"gopath"
+    (gopath/"src/mynewt.apache.org/newt").install contents
+    ENV["GOPATH"] = gopath
+    ENV.prepend_create_path "PATH", gopath/"bin"
+
+    cd gopath/"src/mynewt.apache.org/newt/newt" do
+      system "go", "install"
+      bin.install gopath/"bin/newt"
+    end
+  end
+
+  test do
+    # Compare newt version string
+    assert_equal "1.0.0", shell_output("#{bin}/newt version").split.last
+  end
+end
