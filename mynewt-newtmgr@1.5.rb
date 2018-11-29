3b2d7c153003784df4dce369aa755e9b842b59fe
Fabio Utzig <utzig@apache.org>
2018-11-29 09:20:59 -0200

    build mac os installations for 1.5.0

--- mynewt-newtmgr@1.5.rb
+++ mynewt-newtmgr@1.5.rb
@@ -1,16 +1,16 @@
-class MynewtNewtmgr < Formula
+class MynewtNewtmgrAT15 < Formula
   desc "Tool to manage devices running Mynewt OS via the Newtmgr Protocol"
   homepage "https://mynewt.apache.org"
   url "https://github.com/apache/mynewt-newtmgr/archive/mynewt_1_5_0_tag.tar.gz"
   version "1.5.0"
   sha256 "00bab2c0ea52dab5386b027c92b34936a18ae8ff6a18536db1296fe1e3469832"
 
-  head "https://github.com/apache/mynewt-newtmgr.git"
+  keg_only :versioned_formula
 
   bottle do
      root_url "https://github.com/runtimeco/binary-releases/raw/master/mynewt-newt-tools_1.5.0"
      cellar :any_skip_relocation
-    sha256 "d86b55da7e337f1c9459dc175e6ca5a5f411d1b4fe0df3819b1180ea9704e571" => :sierra
+    sha256 "018522a2ee59181b1f5ecde9db92ff5dee1155042a2fa289f243c5fa4613b337" => :sierra
   end
 
   depends_on "go" => :build
@@ -23,10 +23,10 @@ class MynewtNewtmgr < Formula
     ENV["GOPATH"] = gopath
     ENV.prepend_create_path "PATH", gopath/"bin"
 
-# We are not able to vendor these packages due to a "go get" bug in 
+# We are not able to vendor these packages due to a "go get" bug in
 # vendoring packages with platform dependent code. So we have to get
 # these packages for the buid.
-  
+
     cd gopath/"src" do
        system "go", "get", "github.com/currantlabs/ble"
        system "go", "get", "github.com/raff/goble"
