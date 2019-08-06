c02e5b3a6bf8933b67a4c727160da09c1040e997
Christopher Collins <ccollins@apache.org>
2019-08-06 12:14:01 -0700

    build mac os installations for 1.7.0

--- mynewt-newt@1.7.rb
+++ mynewt-newt@1.7.rb
@@ -1,11 +1,11 @@
-class MynewtNewt < Formula
+class MynewtNewtAT17 < Formula
   desc "Package, build and installation system for Mynewt OS applications"
   homepage "https://mynewt.apache.org"
   url "https://github.com/apache/mynewt-newt/archive/mynewt_1_7_0_tag.tar.gz"
   version "1.7.0"
   sha256 "6aefb5aaae06c4b9d514333ec682f62bd52e97e9d8e7b9f9c50e15983590f1f7"
 
-  head "https://github.com/apache/mynewt-newt.git"
+  keg_only :versioned_formula
 
   bottle do
     root_url "https://github.com/JuulLabs-OSS/binary-releases/raw/master/mynewt-newt-tools_1.7.0"
--- mynewt-newtmgr@1.7.rb
+++ mynewt-newtmgr@1.7.rb
@@ -1,11 +1,17 @@
-class MynewtNewtmgr < Formula
+class MynewtNewtmgrAT17 < Formula
   desc "Tool to manage devices running Mynewt OS via the Newtmgr Protocol"
   homepage "https://mynewt.apache.org"
   url "https://github.com/apache/mynewt-newtmgr/archive/mynewt_1_7_0_tag.tar.gz"
   version "1.7.0"
   sha256 "7aaa8c1c6899a9f39957b51e69d02d5c202a58fa104efcdc5bae75d4691b8c2f"
 
-  head "https://github.com/apache/mynewt-newtmgr.git"
+  keg_only :versioned_formula
+
+  bottle do
+     root_url "https://github.com/JuulLabs-OSS/binary-releases/raw/master/mynewt-newt-tools_1.7.0"
+     cellar :any_skip_relocation
+    sha256 "8f772a9cb14018f268effff2c33c492f1894416178dbb6c390e502c3e91ff6bc" => :high_sierra
+  end
 
   depends_on "go" => :build
   depends_on :arch => :x86_64
